1d52a4274eb6c9f8cafe1b96c92aaf3993665833
Andrew Martin <andrew@believelabs.com>
2017-05-29 14:40:11 -0700

    correctly setting parameters for action creator

--- src/action.js
+++ src/action.js
@@ -1,6 +1,6 @@
 export const TOGGLE_MENU = 'TOGGLE_MENU';
 
-const toggleMenu = (isOpen = false, menuId) => {
+const toggleMenu = ({ isOpen = false, menuId }) => {
   return {
     type: TOGGLE_MENU,
     payload: menuId ? { isOpen, menuId } : { isOpen }
